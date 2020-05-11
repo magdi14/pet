7cf23ee71fbfd4b59eb3b344adba1c73f119dca8
magdi <mohamed.magdi@kijamii.com>
2020-05-11 21:54:15 +0200

    get rid of unwanted files

--- database/models/Permission.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const permission = new Schema({
-  title: String,
-});
-
-module.exports = mongoose.model("Permission", permission);
--- database/models/Role.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const role = new Schema({
-  title: String,
-  permissions: [{ type: Schema.Types.ObjectId, ref: "Permission" }]
-});
-
-module.exports = mongoose.model("Role", role);
